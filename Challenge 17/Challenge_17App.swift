17232dcc6dedd4af9506a32ed765675f22897048
whopes <hopes.w@etoncollege.org.uk>
2022-11-18 16:54:59 +0000

    Initial Commit

---
+++ Challenge 17/Challenge_17App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_17App.swift
+//  Challenge 17
+//
+//  Created by Hopes, William (AMM) on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge 17/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge 17
+//
+//  Created by Hopes, William (AMM) on 18/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
